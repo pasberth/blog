c69420caff3f2186f24c425c6c926ca17948b910
pasberth <pasberth@gmail.com>
2013-10-17 18:33:31 +0900

    init

---
+++ conf.py
@@ -0,0 +1,18 @@
+extensions = []
+
+templates_path = []
+source_suffix = '.rst'
+master_doc = 'index'
+project = u'Blog'
+copyright = u'2013, Pasberth'
+version = '0.0.1'
+release = '0.0.1'
+
+html_title = "pasberth.github.io/blog"
+html_short_title = html_title
+
+html_theme = "pasberth"
+html_theme_path = ['theme']
+html_theme_options = {
+  "nosidebar": True
+}
